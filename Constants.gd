50674e8befcb2dc4c10e755e202411ada65df4cb
Amon Ahmed <Amon.Shokhin.Ahmed@gmail.com>
2021-10-03 19:15:42 +0200

    Add gameplay record constants

--- Constants.gd
+++ Constants.gd
@@ -1,16 +1,29 @@
 extends Node
 
-
 var config
 
+
+const NONE = 0
+
+const DASH_START = 1
+const DASH_END = 2
+
+#TODO: connect weapon information recording with actuall weapon system when ready
+const MELEE_START = 1
+const WEAPON_START = 2
+const MELEE_END = 3
+const WEAPON_END = 4
+
+const GUN = 0
+const WALL = 1
 
 func _init():
 	config = ConfigFile.new()
 	var err = config.load("res://Shared/constants.ini")
-	
+
 	if err != OK:
 		Logger.error("Could not load shared config file!")
 
 
-func get_value(section, key, default=null):
+func get_value(section, key, default = null):
 	return config.get_value(section, key, default)
