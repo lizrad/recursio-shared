84ea0d8e8fa56e629d4f164d9531ac0c93ff41bb
David Panagiotopulos <panagiotopulos.d@gmail.com>
2021-10-07 15:06:49 +0200

    Add ghost to first frame setter

--- Ghost.gd
+++ Ghost.gd
@@ -25,6 +25,11 @@ func _physics_process(delta):
 		_apply_frame(_record["F"][_current_frame])
 		_current_frame+=1
 
+
+func move_to_start_position():
+	_apply_frame(_record["F"][0])
+
+
 #TODO: use move_and_slide for this
 #TODO: also apply weapon and melee attacks
 func _apply_frame(frame: Dictionary):
