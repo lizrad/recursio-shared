7996d14594d8c30d19acd3f4e061b1a2934eba83
Karl <karl.bittner@hexaquo.at>
2021-10-07 12:50:17 +0200

    Add Placeholder Capture Points to Level
    allows Client/Server to easily replace them with the proper scenes

--- Level/Level.gd
+++ Level/Level.gd
@@ -1,6 +1,21 @@
 extends Spatial
 
 
+# Scene to add  to the capture points for logic
+export var capture_point_scene: PackedScene
+
+
+func _ready():
+	if not capture_point_scene:
+		Logger.error("No Capture Point Scene! Capture Points will not work")
+		return
+	else:
+		for point in $CapturePoints.get_children():
+			var new_scene = capture_point_scene.instance()
+			new_scene.global_transform = point.global_transform
+			add_child(new_scene)
+
+
 func get_spawn_positions(player_number):
 	var node_name = "Player" + str(player_number) + "Spawns"
 	if has_node(node_name):
