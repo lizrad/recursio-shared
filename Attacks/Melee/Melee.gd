4228d4247059d09a816633519fda12a51d33ac50
incredibleLeitman <michael.leithner@gmail.com>
2021-10-07 13:29:32 +0200

    transfer basic melee and hitscan attack from prototype

---
+++ Attacks/Melee/Melee.gd
@@ -0,0 +1,94 @@
+extends Area
+
+# TODO: combine attack common values
+export var only_hit_one_body := true
+var _damage := 10.0
+var _bounce_strength := 250.0
+var _attack_range := 1
+var _attack_time := 0.1
+var _owning_player
+var _hit_something = false
+var _continuously_damaging := false
+var _damage_invincibility_time := 0
+var _hit_bodies_invincibilty_tracker = {}
+
+func initialize_visual() -> void:
+	#_attack_time = attack_type.attack_time
+	
+	# TODO: could setup shape statically because does not change
+	var sphereShape = SphereShape.new()
+	sphereShape.radius = _attack_range
+	$CollisionShape.shape = sphereShape
+	$Visualization.scale = Vector3(_attack_range, _attack_range, _attack_range)
+	#$Visualization.get_surface_material(0).albedo_color = Constants.character_colors[owning_player.id]
+	$Visualization.get_surface_material(0).albedo_color = Color.red
+
+#func initialize(owning_player, attack_type) ->void:
+func initialize(owning_player) -> void:
+	#_damage = attack_type.damage
+	#_bounce_strength = attack_type.bounce_strength
+	#_continuously_damaging = attack_type.continuously_damaging
+	#_damage_invincibility_time = attack_type.damage_invincibility_time
+	_owning_player = owning_player
+	initialize_visual()
+
+func _process(delta):
+	_attack_time -= delta
+	if _attack_time <= 0:
+		queue_free()
+		return
+
+	for i in _hit_bodies_invincibilty_tracker:
+		_hit_bodies_invincibilty_tracker[i]-=delta
+		_hit_bodies_invincibilty_tracker[i] = clamp(_hit_bodies_invincibilty_tracker[i],0,_damage_invincibility_time)
+
+func _hit_body(body) ->void:
+	if body.is_in_group("Damagable"):
+			assert(body.has_method("receive_hit"))
+			if (not _hit_bodies_invincibilty_tracker.has(body) or _hit_bodies_invincibilty_tracker[body] <=0):
+				_hit_bodies_invincibilty_tracker[body]=_damage_invincibility_time
+				print(str("Melee attack hit body named ",body.name))
+				_hit_something = true
+				#body.receive_hit(AttackType.AttackTypeType.NORMAL, _damage, (body.global_transform.origin-global_transform.origin).normalized()*_bounce_strength)
+
+func _physics_process(delta):
+	var bodies = get_overlapping_bodies()
+	if bodies.size() == 0:
+		return
+	if not _continuously_damaging:
+		if _hit_something:
+			return
+		var nearest_body
+		var nearest_distance = INF
+		for body in bodies:
+			if _hit_something:
+				return
+			if not body.is_in_group("Damagable"):
+				continue
+			if body == _owning_player:
+				continue
+			var distance = global_transform.origin.distance_to(body.global_transform.origin)
+			if distance < nearest_distance:
+				nearest_distance = distance
+				nearest_body = body
+		if nearest_body == null:
+			return
+		_hit_body(nearest_body)
+	else:
+		var other_player
+		print("hit bodies size "+str(bodies.size()))
+		for body in bodies:
+			if not body.is_in_group("Damagable"):
+				continue
+			if body == _owning_player:
+				continue
+			print("hit "+body.name)
+			# make sure we hit other player last
+			if body.is_player():
+				print ("found other player")
+				other_player = body
+				continue
+			_hit_body(body)
+		if other_player:
+			print ("hit other player")
+			_hit_body(other_player)
---
+++ Attacks/Shots/HitscanShot.gd
@@ -0,0 +1,81 @@
+extends RayCast
+
+# TODO: combine attack common values
+var _continuosly_damaging := false
+var _damage_invincibility_time := 1.1
+var _bullet_range := 50
+var _attack_time := 0.5
+var _owning_player
+var _damage := 10.0
+var _bounce_strength := 50.0
+var _visual := false
+var _first_frame := true
+var _hit_bodies_invincibilty_tracker = {}
+var _attack_type_type
+
+# TODO: use LineRenderer to visualize
+
+func _init() -> void:
+	Logger.info("_init action", "HitscanShot")
+
+#func initialize(owning_player, attack_type) ->void:
+func initialize(owning_player) -> void:
+	_visual = true
+	#_damage = attack_type.damage
+	#_bounce_strength = attack_type.bounce_strength
+	#_bullet_range = attack_type.attack_range
+	#_continuosly_damaging = attack_type.continuously_damaging
+	#_damage_invincibility_time = attack_type.damage_invincibility_time
+	cast_to = Vector3(0,0,-_bullet_range)
+	#_attack_time = attack_type.attack_time
+	_owning_player = owning_player
+	add_exception(owning_player)
+	#_attack_type_type = attack_type.attack_type_type
+
+	#$LineRenderer.points = []
+	#$LineRenderer.points.append(Vector3.ZERO)
+	#$LineRenderer.points.append(Vector3.ZERO)
+	#$LineRenderer.material_override.albedo_color = Constants.character_colors[_owning_player.id]
+	_update_collision()
+	
+func _physics_process(delta):
+
+	_attack_time -= delta
+	if _attack_time <= 0:
+		queue_free()
+		Logger.info("freeing action..." , "HitscanShot")
+		return
+
+	for i in _hit_bodies_invincibilty_tracker.keys():
+		_hit_bodies_invincibilty_tracker[i] -= delta
+		_hit_bodies_invincibilty_tracker[i] = clamp(_hit_bodies_invincibilty_tracker[i],0,_damage_invincibility_time)
+
+	if _visual or (not _first_frame and not _continuosly_damaging):
+		var collider = get_collider()
+		if collider:
+			Logger.info("hit collider: %s" %[collider.get_class()] , "HitscanShot")
+	#		var hit_point = to_local(get_collision_point())
+	#		$LineRenderer.points[1] = hit_point if hit_point.length()<_bullet_range else Vector3(0,0,-_bullet_range)
+	#	else:
+	#		$LineRenderer.points[1] = Vector3(0,0,-_bullet_range)
+	#	$LineRenderer.material_override.albedo_color = Constants.character_colors[_owning_player.id]
+	elif _continuosly_damaging or _first_frame:
+		_first_frame = false
+		_update_collision()
+
+
+func _update_collision():
+	var collider = get_collider()
+	if collider:
+		# Update the line renderer no matter what it is
+		var hit_point = to_local(get_collision_point())
+		#$LineRenderer.points[1] = hit_point if hit_point.length()<_bullet_range else Vector3(0,0,-_bullet_range)
+		
+		# Shoot player if it was damagable
+		if collider.is_in_group("Damagable"):
+			assert(collider.has_method("receive_hit"))
+			if (not _hit_bodies_invincibilty_tracker.has(collider) or _hit_bodies_invincibilty_tracker[collider] <=0):
+				_hit_bodies_invincibilty_tracker[collider]=_damage_invincibility_time
+				collider.receive_hit(_attack_type_type, _damage,-transform.basis.z*_bounce_strength)
+	#else:
+	#	$LineRenderer.points[1] = Vector3(0,0,-_bullet_range)
